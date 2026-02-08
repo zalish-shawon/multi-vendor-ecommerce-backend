dca6087da5680c99590af816fdd2c234e461272d
zalish-shawon <zalish.shawon@gmail.com>
2026-02-08 15:23:44 +0600

    added

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,29 +1,40 @@
-import express from 'express';
-import { register, login, updateProfile, addAddress, deleteAddress, setDefaultAddress, updatePassword, vendorLogin, verifyOtp, resendOtp } from '../controllers/authController';
-import { upload } from '../config/cloudinary';
-import { verifyToken } from '../middleware/authMiddleware';
+import express from "express";
+import {
+  register,
+  login,
+  updateProfile,
+  addAddress,
+  deleteAddress,
+  setDefaultAddress,
+  updatePassword,
+  vendorLogin,
+  verifyOtp,
+  resendOtp,
+} from "../controllers/authController";
+import { upload } from "../config/cloudinary";
+import { verifyToken } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post("/register", register);
+router.post("/login", login);
 
-router.put('/profile', verifyToken, upload.array('image', 1), updateProfile);
+router.put("/profile", verifyToken, upload.array("image", 1), updateProfile);
 
 // Address Management
-router.post('/address', verifyToken, addAddress);
-router.delete('/address/:addressId', verifyToken, deleteAddress);
+router.post("/address", verifyToken, addAddress);
+router.delete("/address/:addressId", verifyToken, deleteAddress);
 
-router.put('/address/:addressId/default', verifyToken, setDefaultAddress);
+router.put("/address/:addressId/default", verifyToken, setDefaultAddress);
 
 // Change Password
-router.put('/profile/password', verifyToken, updatePassword);
+router.put("/profile/password", verifyToken, updatePassword);
 
-router.post('/vendor/login', vendorLogin); // New separate login route
+router.post("/vendor/login", vendorLogin); // New separate login route
 
 // OTP Verification
-router.post('/verify-otp', verifyOtp);
+router.post("/verify-otp", verifyOtp);
 // Resend OTP
-router.post('/resend-otp', resendOtp);
+router.post("/resend-otp", resendOtp);
 
 export default router;
