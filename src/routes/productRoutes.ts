b8b21b63acee7bd6cbb6d282ac7f843b8a0fe5b1
zalish-shawon <zalish.shawon@gmail.com>
2026-02-15 10:55:47 +0600

    refactor codes

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,19 +1,32 @@
-import express from 'express';
-import { AdminCreateProduct, AdminDeleteProduct, AdminUpdateProduct, createProduct, deleteProduct, getAllProducts, getProductById, updateProduct } from '../controllers/productController';
-import { verifyToken, isVendor, isAdmin } from '../middleware/authMiddleware';
-import { upload } from '../config/cloudinary';
+import express from "express";
+import {
+  AdminCreateProduct,
+  AdminDeleteProduct,
+  AdminUpdateProduct,
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+} from "../controllers/productController";
+import { verifyToken, isVendor, isAdmin } from "../middleware/authMiddleware";
+import { upload } from "../config/cloudinary";
 
 const router = express.Router();
 
 // Public Route: Anyone can see products
-router.get('/', getAllProducts);
+router.get("/", getAllProducts);
 // Public Route: Anyone can see a single product by ID
-router.get('/:id', getProductById);
+router.get("/:id", getProductById);
 // Protected Route: Only Logged-in Vendors can add products
-router.post('/add', verifyToken, isVendor, upload.array('images', 5), createProduct);
-router.put('/:id', verifyToken, isVendor, updateProduct);
-router.delete('/:id', verifyToken, isVendor, deleteProduct);
-
-
+router.post(
+  "/add",
+  verifyToken,
+  isVendor,
+  upload.array("images", 5),
+  createProduct,
+);
+router.put("/:id", verifyToken, isVendor, updateProduct);
+router.delete("/:id", verifyToken, isVendor, deleteProduct);
 
 export default router;
